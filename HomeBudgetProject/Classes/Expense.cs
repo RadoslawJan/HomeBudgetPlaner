bcdfa578f31280c4231e3395008dc6b0fb10f223
Bartek-Orl <bartosz.orl@wp.pl>
2026-01-03 17:14:18 +0100

    wydatki

--- HomeBudgetProject/Classes/Expense.cs
+++ HomeBudgetProject/Classes/Expense.cs
@@ -1,16 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HomeBudgetProject.Enums;
 
 namespace HomeBudgetProject.Classes
 {
     public class Expense : BudgetItem
     {
+        public Expense(string name, float value, CategoryType category)
+        {
+            this.Name = name;
+            this.Value = value;
+            this.Category = category;
+        }
         public override float GetValue()
         {
-            throw new NotImplementedException();
+            return Value;
         }
 
     }
