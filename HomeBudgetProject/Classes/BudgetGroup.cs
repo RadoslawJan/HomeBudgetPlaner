38b6107f942da6de1a0fb77a98a629780055c3e9
Bartek-Orl <bartosz.orl@wp.pl>
2026-01-03 17:08:23 +0100

    Dodanie kategorii

--- HomeBudgetProject/Classes/BudgetGroup.cs
+++ HomeBudgetProject/Classes/BudgetGroup.cs
@@ -1,14 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HomeBudgetProject.Enums;
 
 namespace HomeBudgetProject.Classes
 {
     public class BudgetGroup : BudgetItem
     {
         public List<BudgetItem> budgetItemList = new List<BudgetItem>();
+
+        public BudgetGroup(CategoryType category) {
+            this.Category = category;
+            this.Name = category.ToString();
+            this.Value = 0;
+        }
+
         public void Add(BudgetItem item)
         {
             budgetItemList.Add(item);
@@ -20,8 +28,12 @@ namespace HomeBudgetProject.Classes
         }
         public override float GetValue()
         {
-            throw new NotImplementedException();
-            // Tu dodać sumowanie z listy np przez linq
+            float total = 0;
+            foreach (var item in budgetItemList)
+            {
+                total += item.GetValue();
+            }
+            return total;
         }
     }
 }
